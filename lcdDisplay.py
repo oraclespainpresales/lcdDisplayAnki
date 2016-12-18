1359b571c61a8350b9b029e6b06ee532e38ef1a2
Carlos Casares <ccasares@gmail.com>
2016-12-18 20:41:48 +0100

    event

--- lcdDisplay.py
+++ lcdDisplay.py
@@ -129,6 +129,7 @@ def sync_bics():
 def get_current_event():
   currentdate = time.strftime("%m-%d-%Y")
   url = get_dbcs() + "/apex/pdb1/anki/events/" + currentdate
+  print url
   currentevent = getRest("", url)
   if currentevent.status_code == 200:
     data = json.loads(currentevent.content)
