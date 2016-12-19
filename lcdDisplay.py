d0f769e14dae071ca695003be902e8402cf62c37
Carlos Casares <ccasares@gmail.com>
2016-12-19 23:43:38 +0100

    remove debug

--- lcdDisplay.py
+++ lcdDisplay.py
@@ -784,7 +784,6 @@ def check_reverse_proxy():
   global proxyport
   URI = CHECK_REVERSEPROXY_CMD
   URI = URI.replace("{DRONEPORT}", proxyport)
-  print URI
   listeners=int(run_cmd(URI))
   if listeners > 0:
      return "OK"
@@ -795,14 +794,12 @@ def check_nodejs():
    global proxyport
    URI = CHECK_NODEUP_CMD
    URI = URI.replace("{DRONEPORT}", proxyport)
-   print URI
    return run_cmd(URI)
 
 def check_websocket():
    global proxyport
    URI = CHECK_WEBSOCKET_CMD
    URI = URI.replace("{DRONEPORT}", proxyport)
-   print URI
    return run_cmd(URI)
 
 def getPiName():
